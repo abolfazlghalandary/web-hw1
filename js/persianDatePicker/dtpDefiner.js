323c74c404b1eea1b4c19e0cf558ae68abdde3e0
mohammadKhalafi <mohamadkhalafi.81@gmail.com>
2022-12-01 04:31:45 +0330

    persian calender

---
+++ js/persianDatePicker/dtpDefiner.js
@@ -0,0 +1,8 @@
+const dtp1 = new mds.MdsPersianDateTimePicker(document.getElementById('dtp1'), {
+  targetTextSelector: '[data-name="dtp1-text"]',
+  targetDateSelector: '[data-name="dtp1-date"]',
+});
+const dtp2 = new mds.MdsPersianDateTimePicker(document.getElementById('dtp2'), {
+  targetTextSelector: '[data-name="dtp2-text"]',
+  targetDateSelector: '[data-name="dtp2-date"]',
+});
