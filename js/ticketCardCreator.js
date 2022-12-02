425db5d80537a570cd1e8feecaf9c461959d8a7b
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2022-12-02 22:18:15 +0330

    implement ticket search

--- js/ticketCardCreator.js
+++ js/ticketCardCreator.js
@@ -1,10 +1,16 @@
-function addToTickets() {
-  let tickets = document.getElementById("tickets");
-
-  let opt = document.createElement('div');
-  opt.innerHTML = "
-
-    ";
-
-  element.appendChild(opt);
+let tickets = document.getElementById("tickets");
+let ticketTemplate = document.querySelector('#ticketTemplate');
+function addToTickets(ticketInfo) {
+  let clone = ticketTemplate.cloneNode( true );
+  clone.querySelector("#class").innerText = ticketInfo.flightClass
+  clone.querySelector("#flightSource").innerText = ticketInfo.sourceLocation
+  clone.querySelector("#flightTime").innerText = ticketInfo.flightTime
+  clone.querySelector("#sourceAirport").innerText = ticketInfo.flightClass
+  clone.querySelector("#duration").innerText = ticketInfo.flightClass
+  clone.querySelector("#flightDestination").innerText = ticketInfo.flightClass
+  clone.querySelector("#endTime").innerText = ticketInfo.flightClass
+  clone.querySelector("#destinationAirport").innerText = ticketInfo.flightClass
+  clone.querySelector("#price").innerText = ticketInfo.flightClass
+  tickets.appendChild( clone );
 }
+addToTickets()
