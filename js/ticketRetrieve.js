a06d3c2c50a59364028e58c6f5ec21609e9305c0
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2022-12-08 00:18:26 +0330

    implement default field values

--- js/ticketRetrieve.js
+++ js/ticketRetrieve.js
@@ -8,6 +8,23 @@ let departDate = params.departDate
 let returnDate = params.returnDate
 let passengersCount = params.passengers
 
+
+function SetSearchInfoHeader() {
+  document.querySelector("#source").innerText = sourceLocation
+  document.querySelector("#destination").innerText = destination
+  document.querySelector("#start").innerText = departDate
+  document.querySelector("#end").innerText = returnDate
+  document.querySelector("#passengers").innerText = passengersCount
+}
+
+function SetDefaultSearchFields() {
+  document.querySelector("#from").value = sourceLocation
+  document.querySelector("#to").value = destination
+  document.querySelector("#dtp1Val").value = departDate
+  document.querySelector("#dtp2Val").value = returnDate
+  document.querySelector("#passengersCount").value = passengersCount
+}
+
 for (let ticket of getAllTickets().filter(ticket =>
   ticket.sourceLocation === sourceLocation &&
   ticket.destination === destination &&
@@ -17,8 +34,7 @@ for (let ticket of getAllTickets().filter(ticket =>
   addToTickets(ticket)
 }
 
-document.querySelector("#source").innerText = sourceLocation
-document.querySelector("#destination").innerText = destination
-document.querySelector("#start").innerText = departDate
-document.querySelector("#end").innerText = returnDate
-document.querySelector("#passengers").innerText = passengersCount
+SetSearchInfoHeader();
+SetDefaultSearchFields();
+
+
