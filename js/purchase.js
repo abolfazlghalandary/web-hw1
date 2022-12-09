6193265f8ca38f88abf45464bfdfac1a4a407f8d
ARB1380 <azerila.farshi@gmail.com>
2022-12-09 20:25:39 +0330

    commit

--- js/purchase.js
+++ js/purchase.js
@@ -1,8 +1,42 @@
 function duplicate(){
-  let original = document.getElementById('duplicater');
+  let i = 0;
+  let passengers = document.getElementById('passengerSection').childNodes;
+
+  for(let child in passengers){
+    if(passengers[child].nodeType == 1){
+      i = i + 1;
+    }
+  }
+
+  let original = document.getElementById('passenger_0');
   let copy = original.cloneNode(true);
+  copy.id = 'passenger_' + i;
   let passengerSection = document.getElementById('passengerSection');
   passengerSection.appendChild(copy);
+
+}
+
+function saveInformation(){
+
+  let i = 0;
+  let passengers = document.getElementById('passengerSection').childNodes;
+
+  for(let child in passengers){
+    if(passengers[child].nodeType == 1){
+      i = i + 1;
+    }
+  }
+
+  for(let j = 0; j < i; i ++){
+    let passenger = document.getElementById('passenger_'+ j);
+    let passportNumber = passenger.querySelector("#passportNumber").innerText;
+    let lastName = passenger.querySelector("#lastName").innerText;
+    let firstName = passenger.querySelector("#firstName").innerText;
+
+  }
+
+
+
 
 }
 
