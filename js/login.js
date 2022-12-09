aefcb89dec0b7f3def83d642058652cc0075f5db
ARB1380 <azerila.farshi@gmail.com>
2022-12-09 10:32:24 +0330

    add validation for login register

--- js/login.js
+++ js/login.js
@@ -1,10 +1,24 @@
-function generateRandomToken(){
+function generateRandomToken() {
 
   return Math.random().toString(36).substring(2);
 
 }
 
-function login(){
-  let token = generateRandomToken();
-  sessionStorage.setItem("token", token);
+function login() {
+  let username = localStorage.getItem("username");
+  let password = localStorage.getItem("password");
+  if (validate(username, password)) {
+    let token = generateRandomToken();
+    sessionStorage.setItem("token", token);
+  }
+  else{
+    alert("incorrect username or password")
+  }
+}
+
+function validate(username, password){
+  let inputUsername = document.getElementById('username').value;
+  let inputPassword = document.getElementById('password').value;
+  return username === inputUsername && password === inputPassword;
+
 }
