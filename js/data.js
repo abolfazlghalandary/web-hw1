072e93d0594d039fd7ef0f1d7e64836aa471bae9
Abolfazl Ghalandary <79265051+abolfazlghalandary@users.noreply.github.com>
2022-12-09 18:50:08 +0330

    delete unused links

--- js/data.js
+++ js/data.js
@@ -155,10 +155,3 @@ function getAllTickets(){
     }
   ];
 }
-
-$(document).ready(function() {
-  $('.autoComplete').select2();
-});
-$(document).on('select2:open', () => {
-  document.querySelector('.select2-search__field').focus();
-});
